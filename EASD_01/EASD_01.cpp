63a0ee1535ac7827cb133a3f8d5dae30688556fd
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-04-07 10:05:21 +0700

    pim

--- EASD_01/EASD_01.cpp
+++ EASD_01/EASD_01.cpp
@@ -1,2 +1,4 @@
 //karena alghoritma sangat terperinci dalam hal hal kodingan,dalam menyelesaikan masalah masalah yang detail
 //static dan dynamic
+//data yang tersusun rapi
+//bubble sort,karena bubble sort selama saya belajar aalghoritma,dia yang paling mudah dan simpel
